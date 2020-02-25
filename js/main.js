b875c0687af3cfbcd1aa8a9d36f8b68af9fb7b71
Andrea <33752870+GalliaAndre@users.noreply.github.com>
2020-02-25 17:10:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+             .register('./service-worker.js');
   }
 }
