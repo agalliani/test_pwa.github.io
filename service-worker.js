700b4fc20b4a5181d4a0250e6e3061361c9f1e10
Andrea <33752870+GalliaAndre@users.noreply.github.com>
2020-02-25 16:31:25 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+var CACHE_NAME= 'test_pwa';
+var FILES_TO_CACHE= [
+'/',
+'/index.html',
+'/css/style.css',
+'js/main.js'
+];
+
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
