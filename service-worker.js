eb5a94ba4ef73da4f1c13f02e09472e64c63f71e
GalliaAndre <andrea.galliani.29@gmail.com>
2020-02-27 01:19:12 +0100

    ident

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,59 @@
 var CACHE_NAME= 'test-pwa';
 var FILES_TO_CACHE= [
-'index.html',
-  'main.dart.js',
-  'main.dart.js.map',
-  'assets/AssetManifest.json',
-  'assets/FontManifest.json',
-  'assets/fonts/MaterialIcons-Regular.ttf',
-  'assets/packages/cupertino_icons/assets/CupertinoIcons.ttf'
-];
+    'index.html',
+    'main.dart.js',
+    'main.dart.js.map',
+    'assets/AssetManifest.json',
+    'assets/FontManifest.json',
+    'assets/fonts/MaterialIcons-Regular.ttf',
+    'assets/packages/cupertino_icons/assets/CupertinoIcons.ttf'
+    ];
 
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
-    
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
 });
 
 
 
 /* Serve cached content when offline or new (if there are) when online. STALE-WHILE-REVALIDATE */
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-
-  e.respondWith(
-      
-    /*caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-*/
-      caches.open('CACHE_NAME').then(function(cache) {
-      return cache.match(e.request).then(
-          function(response) {
-              var fetchPromise = fetch(e.request).then(function(networkResponse) {
-                  cache.put(e.request, networkResponse.clone());
-                  return networkResponse;
-              })
-        return response || fetchPromise;
-      })
-    })
-  );
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.open('CACHE_NAME').then(function(cache) {
+            return cache.match(e.request).then(
+                function(response) {
+                    var fetchPromise = fetch(e.request).then(function(networkResponse) {
+                        cache.put(e.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                    return response || fetchPromise;
+                })
+        })
+    );
 });
 
 
 self.addEventListener('activate', (e) => {
-  console.log('[ServiceWorker] Activate');
-  //Remove previous cached data from disk.
-e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
-  self.clients.claim();
+    console.log('[ServiceWorker] Activate');
+
+    //Remove previous cached data from disk.
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
 });
 
